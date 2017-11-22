bd3ad9ed6f4310a1ea3f6a56ca0b3d4cd669a9a4
M.Reddy <mreddy@localstoreidentity.com>
2017-11-21 16:15:08 -0800

    web3 init added to app.js in Dapp

--- Dapp/app/javascripts/app.js
+++ Dapp/app/javascripts/app.js
@@ -9,6 +9,15 @@ import {
     default as contract
 } from 'truffle-contract'
 
+if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source like Metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+} else {
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
 
 /*
  * When you compile and deploy your Copyright contract,
@@ -110,16 +119,6 @@ window.addCopyright = function(docname) {
 }
 
 $(document).ready(function() {
-    if (typeof web3 !== 'undefined') {
-        console.warn("Using web3 detected from external source like Metamask")
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(web3.currentProvider);
-    } else {
-        console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
-
     //Copyright.setProvider(web3.currentProvider);
     // get the length of documents on the chain
     var contractCount = 0;
